86186f4c7cc50c0bc98119249f446a5ac267de54
Rohit Sudhir Patil <rohit.patil1@somaiya.edu>
2021-11-21 22:17:14 +0530

    taking input

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let btnTranslate = document.getElementById('btn-translate');
-let textArea = document.getElementById('in-txt');
-let outputArea = document.getElementById('out-txt');
+let btnTranslate = document.getElementById("btn-translate");
+let textArea = document.getElementById("in-txt");
+let outputArea = document.getElementById("out-txt");
+
+let url = "https://api.funtranslations.com/translate/shakespeare.json";
+
+function serverURL(text) {
+    return url + "?text=" + text;
+}
+
+function clickHandler() {
+    let inputText = textArea.value;
+    let outputText = "";
+}
+
+btnTranslate.addEventListener("click", clickHandler);
