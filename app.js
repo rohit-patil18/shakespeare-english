08319554898efb05322d68de45205270825dea5d
Rohit Sudhir Patil <rohit.patil1@somaiya.edu>
2021-11-21 22:20:43 +0530

    output processing

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ function serverURL(text) {
     return url + "?text=" + text;
 }
 
+function errorHandler(error) {
+    console.log("Error occured!", error);
+    alert("Something wrong with server! Try again after some time.")
+}
+
 function clickHandler() {
     let inputText = textArea.value;
     let outputText = "";
+    fetch(serverURL(inputText))
+        .then((response) => response.json())
+        .then((json) => {
+            outputText = json.contents.translated;
+            outputArea.value = outputText;
+        })
+        .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
